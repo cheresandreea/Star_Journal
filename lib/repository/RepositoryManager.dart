53d43ade5c7fb1637a3b90537477509d53a8146a
922-Cheres-Andreea <115451911+cheresandreea@users.noreply.github.com>
2025-05-06 13:31:13 +0300

    Update RepositoryManager.dart

--- lib/repository/RepositoryManager.dart
+++ lib/repository/RepositoryManager.dart
@@ -5,8 +5,8 @@ import 'package:star_journal/repository/RepositoryDB.dart';
 import 'package:star_journal/NetworkService.dart';
 
 class RepositoryManager implements IRepository {
-  final Repository localRepository; // Local DB
-  final RepositoryDB serverRepository; // Remote DB
+  final Repository localRepository;
+  final RepositoryDB serverRepository; 
 
   RepositoryManager({
     required this.localRepository,
@@ -20,15 +20,12 @@ class RepositoryManager implements IRepository {
 
   @override
   Future<Star> addStar(Star star) async {
-    // Add to local first for offline storage
     final addedStar = await localRepository.addStar(star);
     print('Added star locally: $addedStar');
-    // Optionally, add to remote in the background
     try {
       await serverRepository.addStar(addedStar);
       print('Added star to remote: $addedStar');
     } catch (e) {
-      // Handle failure to add remotely (e.g., log or mark for syncing)
       print('Failed to sync star to remote: $e');
     }
     return addedStar;
@@ -36,13 +33,10 @@ class RepositoryManager implements IRepository {
 
   @override
   Future<void> deleteStar(int id) async {
-    // Delete locally first
     await localRepository.deleteStar(id);
-    // Optionally delete remotely in the background
     try {
       await serverRepository.deleteStar(id);
     } catch (e) {
-      // Handle failure to delete remotely
       print('Failed to delete star from remote: $e');
     }
   }
@@ -54,13 +48,10 @@ class RepositoryManager implements IRepository {
 
   @override
   Future<Star> updateStar(Star star) async {
-    // Update locally first
     final updatedStar = await localRepository.updateStar(star);
-    // Optionally update remotely in the background
     try {
       await serverRepository.updateStar(updatedStar);
     } catch (e) {
-      // Handle failure to update remotely
       print('Failed to update star on remote: $e');
     }
     return updatedStar;
